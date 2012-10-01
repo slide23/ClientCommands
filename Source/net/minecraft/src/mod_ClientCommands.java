b2e90a0f170425fe1eb07df6c006733de5d36e84
slide23 <github@slide.ws>
2012-10-01 18:32:20 -0500

    Initial commit

---
+++ Source/net/minecraft/src/mod_ClientCommands.java
@@ -0,0 +1,62 @@
+package net.minecraft.src;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+
+import ws.slide.minecraft.mod_ClientCommands.ChatCallback;
+import ws.slide.minecraft.mod_ClientCommands.ClientCommandManager;
+import ws.slide.minecraft.mod_ClientCommands.ClientCommandSender;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.src.ModLoader;
+import net.minecraft.src.ICommandSender;
+
+public class mod_ClientCommands extends BaseMod
+{
+	private static mod_ClientCommands instance = null;
+	private ClientCommandManager clientCommandManager = new ClientCommandManager();
+	private ClientCommandSender commandSender;
+	private List<ChatCallback> chatCallbacks = new ArrayList<ChatCallback>();
+
+	public Boolean handleClientCommand(String commandLine)
+	{
+		return clientCommandManager.handleClientCommand(commandLine);
+	}
+
+	public String executeChatCallbacks(String text)
+	{
+		StringBuilder textBuilder = new StringBuilder(text);
+		for (ChatCallback callback : getChatCallbacks())
+			if (callback.execute(textBuilder))
+				break;
+
+		return textBuilder.toString();
+	}
+
+	public mod_ClientCommands()
+	{
+    	instance = this;
+	}
+
+	public static mod_ClientCommands getInstance()
+	{
+		return instance;
+	}
+
+	@Override
+	public String getVersion()
+	{
+		return "v0.9 by slide23, totemo for Minecraft v1.3.2";
+	}
+
+	@Override
+	public void load()
+	{
+	}
+
+	public ClientCommandManager getClientCommandManager() { return clientCommandManager; }
+	public void addChatCallback(ChatCallback callback) { chatCallbacks.add(callback); }
+	public void registerCommand(ICommand par1ICommand) { clientCommandManager.registerCommand(par1ICommand); }
+	public List<ChatCallback> getChatCallbacks() { return chatCallbacks; }
+}
---
+++ Source/ws/slide/minecraft/mod_ClientCommands/ChatCallback.java
@@ -0,0 +1,6 @@
+package ws.slide.minecraft.mod_ClientCommands;
+
+public interface ChatCallback
+{
+	public Boolean execute(StringBuilder line);
+}
